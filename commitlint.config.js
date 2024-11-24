075fa16269797e2c9caea22f3e727276c2cd66f7
Adonis Lee Villamor <adonisv79@gmail.com>
2024-11-24 11:23:13 +0800

    fix: commit body and footer issue

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
+    'body-max-line-length': [2, 'always', 300], // we need to set this as commits of semantic-release can be quite detailed
+    'footer-max-line-length': [0], // we need to set this as commits of semantic-release can be quite detailed
     'subject-case': [0], // Disable subject case rule
   },
 };
